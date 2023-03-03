a7aed76be4d48d20ea4fa91f7c4e132e07a403c7
Neo <lordneobarnachea@gmail.com>
2023-03-04 07:29:02 +0800

    added components

--- admin/src/components/Home.js
+++ admin/src/components/Home.js
@@ -1,40 +1,10 @@
-import {React, useState, useEffect} from 'react';
-import { collection, getDocs } from "firebase/firestore";
 import LogoutBtn from './LogoutBtn';
-import { db } from '../firebase';
+import Certificate from './Certificate';
 const Home = () => {
-    const [data, setData] = useState([]);
-
-     useEffect(() => {
-    const fetchData = async () => {
-      const querySnapshot = await getDocs(collection(db, "CertificateForm"));
-      const documents = querySnapshot.docs.map((doc) => doc.data());
-      setData(documents);
-    };
-
-    fetchData();
-  }, []);
-
-      return(
-    <div>
-      <h1>Fetch data from CertificateForm</h1>
-      <ul>
-        {data.map((doc) => (
-          <li>
-            <h4>{doc.certificate}</h4>
-            <h4>{doc.email}</h4>
-            <h4>{doc.firstname}</h4>
-            <h4>{doc.lastname}</h4>
-            <h4>{doc.phone}</h4>
-          </li>
-          
-         
-
-
-        ))}
-      </ul>
-    </div>
-  );
+  return(
+    <Certificate/>
+  )    
+  
 } 
           
     
